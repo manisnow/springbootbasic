3885892cd9cede738414393f2c049289d5a9e959
manisnow <manisnow@gmail.com>
2019-06-29 02:15:00 -0400

    added the swagger2

--- src/main/java/com/mt/demo/Application.java
+++ src/main/java/com/mt/demo/Application.java
@@ -6,7 +6,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
@@ -18,5 +28,26 @@ public class Application {
 
 		return KieServices.Factory.get().getKieClasspathContainer();
 	}
+	
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+            .apis(RequestHandlerSelectors
+                .basePackage("com.mt.demo.controller"))
+            .paths(PathSelectors.regex("/.*"))
+            .build().apiInfo(apiEndPointsInfo());
+    }
+	
+	
+	 private ApiInfo apiEndPointsInfo() {
+	        return new ApiInfoBuilder().title("Spring Boot REST API")
+	            .description("Spring boot basic REST API")
+	            .contact(new Contact("Manikandan", "mindtraits.com	", "manisnow@gmail.com"))
+	            .license("Apache 2.0")
+	            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+	            .version("1.0.0")
+	            .build();
+	    }
+	
 
 }
